580e1a066d737469941e5b70f6a68ebc1ef42c17
Moana <missn.hadebe@gmail.com>
2026-01-11 15:02:16 +0200

    add signals to invalidate all_properties cache on Property save/delete

--- properties/apps.py
+++ properties/apps.py
@@ -3,3 +3,14 @@ from django.apps import AppConfig
 
 class PropertiesConfig(AppConfig):
     name = 'properties'
+
+
+
+from django.apps import AppConfig
+
+class PropertiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'properties'
+
+    def ready(self):
+        import properties.signals
