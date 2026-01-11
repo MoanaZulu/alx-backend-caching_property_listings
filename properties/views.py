882e96ad0531b5dc3a0f241a1eedcd4a6ee44038
Moana <missn.hadebe@gmail.com>
2026-01-11 14:15:59 +0200

    Add getallproperties caching function and update property_list view

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+from django.shortcuts import render
+from .utils import getallproperties
+
+def property_list(request):
+    properties = getallproperties()
+    return render(request, 'properties/property_list.html', {'properties': properties})
