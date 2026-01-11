b823251dfedfb8958bbfb88f693efbc90e9fcdde
Moana <missn.hadebe@gmail.com>
2026-01-11 14:40:24 +0200

    Add getallproperties and update property_list

--- properties/views.py
+++ properties/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-
-
-from django.shortcuts import render
 from .utils import getallproperties
 
 def property_list(request):
     properties = getallproperties()
     return render(request, 'properties/property_list.html', {'properties': properties})
 
-
-
-from django.shortcuts import render
-from .utils import getallproperties
-
-def property_list(request):
-    properties = getallproperties()
-    return render(request, 'properties/property_list.html', {'properties': properties})
