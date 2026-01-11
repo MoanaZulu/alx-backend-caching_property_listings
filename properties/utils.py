00f8b3875d34303897477c0437d3e4f10a4bc7ee
Moana <missn.hadebe@gmail.com>
2026-01-11 14:29:43 +0200

    Add getallproperties and update property_list

--- properties/utils.py
+++ properties/utils.py
@@ -12,3 +12,20 @@ def getallproperties():
         cache.set('allproperties', properties, 3600)
 
     return properties
+
+
+
+from django.core.cache import cache
+from .models import Property
+
+def getallproperties():
+    # Try to get cached queryset
+    properties = cache.get('allproperties')
+
+    if properties is None:
+        # Cache miss: fetch from DB
+        properties = Property.objects.all()
+        # Store in Redis for 1 hour (3600 seconds)
+        cache.set('allproperties', properties, 3600)
+
+    return properties
--- properties/views.py
+++ properties/views.py
@@ -10,3 +10,12 @@ from .utils import getallproperties
 def property_list(request):
     properties = getallproperties()
     return render(request, 'properties/property_list.html', {'properties': properties})
+
+
+
+from django.shortcuts import render
+from .utils import getallproperties
+
+def property_list(request):
+    properties = getallproperties()
+    return render(request, 'properties/property_list.html', {'properties': properties})
