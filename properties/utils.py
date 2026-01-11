fdabdd8d26a218ef0968ec93f1abb6c7ae0fb851
Moana <missn.hadebe@gmail.com>
2026-01-11 15:08:43 +0200

    Implement get_all_properties with Redis caching and update property_list

--- properties/utils.py
+++ properties/utils.py
@@ -45,3 +45,15 @@ def get_redis_cache_metrics():
             "keyspace_misses": 0,
             "hit_ratio": 0,
         }
+
+
+
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    properties = cache.get('all_properties')
+    if properties is None:
+        properties = Property.objects.all()
+        cache.set('all_properties', properties, 3600)
+    return properties
--- properties/views.py
+++ properties/views.py
@@ -5,3 +5,12 @@ def property_list(request):
     properties = getallproperties()
     return render(request, 'properties/property_list.html', {'properties': properties})
 
+
+
+
+from django.shortcuts import render
+from .utils import get_all_properties
+
+def property_list(request):
+    properties = get_all_properties()
+    return render(request, 'properties/property_list.html', {'properties': properties})
