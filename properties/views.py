6595b9f12cd0a47923be0771db8f9c73e210a4cb
Moana <missn.hadebe@gmail.com>
2026-01-11 15:17:52 +0200

    Add property_list view with @cache_page and map to /properties/

--- properties/views.py
+++ properties/views.py
@@ -14,3 +14,16 @@ from .utils import get_all_properties
 def property_list(request):
     properties = get_all_properties()
     return render(request, 'properties/property_list.html', {'properties': properties})
+
+
+
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
+
+@cache_page(60 * 15)
+def property_list(request):
+    properties = list(Property.objects.values())
+    return JsonResponse({
+        "data": properties
+    })
